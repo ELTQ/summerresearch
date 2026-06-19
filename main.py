d3ad0e778eb1e4fa80e27edcefa4e7a1ce7b02e6
Tianyu Qiu <139846244+ELTQ@users.noreply.github.com>
2026-06-18 20:00:23 -0600

    Enhance circle comparison and tree splitting
    Added functionality to compare circles and split the tree.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sdflib.sdfs import * 
 import random
 import vpt.tree as tree
+from vpt.tree import VPTree, mse
 
 circ1 = SDF(func=SDF.circle, ID=1)
 circ2 = SDF(func=SDF.circle, ID=2)
+circf = SDF(func=SDF.circle, ID=6)
 
 pointx = []
 pointy = []
@@ -11,3 +13,12 @@ points = []
 for i in range(5):
     points.append((random.randint(-5, 5), random.randint(-5, 5)))
 print("the difference between circles is", circ1.compare(circ2, points))
+
+
+circs = [circ1, circ2, circf]
+
+ucirc = SDF(func=SDF.circle, ID=1)
+
+tree = VPTree(points, circs, ucirc)
+tree.split()
+print(tree.left.sdfs, tree.right.sdfs)
