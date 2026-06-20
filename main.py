7e4e8b1c1f65d8d43c31a67b0a5b53dcac5e0b79
Drew Smith <drewsmith907@icloud.com>
2026-06-20 17:54:29 -0600

    added tests for our SDF generation

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 from sdflib.sdfs import * 
+from sdflib.generate_sdfs import *
 import random
 import vpt.tree as tree
 from vpt.tree import VPTree, mse
 
-circ1 = SDF(func=SDF.circle, ID=1)
-circ2 = SDF(func=SDF.circle, ID=2)
-circ3 = SDF(func=SDF.circle, ID=3)
-circ4 = SDF(func=SDF.circle, ID=4)
-circ5 = SDF(func=SDF.circle, ID=5)
-circ6 = SDF(func=SDF.circle, ID=6)
 
 pointx = []
 pointy = []
-points = []
-for i in range(5):
-    points.append((random.randint(-5, 5), random.randint(-5, 5)))
-print("the difference between circles is", circ1.compare(circ2, points))
+points = [(random.randint(-300, 300), random.randint(-300, 300)) for i in range(20)]
 
+circs = generate_circles(100)
 
-circs = [circ1, circ2, circ3, circ4, circ5, circ6]
-
-ucirc = SDF(func=SDF.circle, ID=1)
+ucirc = SDF(func=SDF.circle, cx=0, cy=0)
 
 tree = VPTree(points, circs, ucirc)
 tree.split()
