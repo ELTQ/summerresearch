d60e370e5c7d3a7afe19eba374328da6c558a521
Drew Smith <drewsmith907@icloud.com>
2026-06-24 00:50:24 -0600

    added constants

--- dataviz.py
+++ dataviz.py
@@ -7,6 +7,12 @@ import networkx as nx
 import random 
 import matplotlib.pyplot as plt
 
+NUM_CIRCLES = 50
+NUM_TRIANGLES = 50
+FIND_NEARBY = 10
+SUCCESS_COLOR = "lightgreen"
+FAIL_COLOR = "maroon"
+
 def connectToTree(gr, tree):
     cur_node = tree
     near_child = tree.near
@@ -21,7 +27,7 @@ def connectToTree(gr, tree):
             gr.add_edge(cur_node.sdf.report(), near_child.sdf.report())
         
         gr = connectToTree(gr, near_child)
-        
+
     if (far_child and far_child.threshold != -1 ):
         if far_child.leaf:
             for leafling in far_child.leaf:
@@ -42,10 +48,11 @@ pointx = []
 pointy = []
 points = [(random.randint(-300, 300), random.randint(-300, 300)) for i in range(20)]
 
-shapes = generate_circles(50)
-tris = generate_triangles(50)
+shapes = generate_circles(NUM_CIRCLES)
+tris = generate_triangles(NUM_TRIANGLES)
 
-shapes.append( sdfs.SDF(sdfs.SDF.circle, 10000000000, 100000000))
+
+# shapes.append( sdfs.SDF(sdfs.SDF.circle, 10000000000, 100000000))
 
 shapes += (tris)
 
@@ -63,8 +70,9 @@ print(gr)
 
 target = generate_circles(1)[0]
 print("target is", target)
-nearest_hits = [0] * 5
-full_result_knn = tree.searchkNN(target, 5)
+
+nearest_hits = [0] * FIND_NEARBY
+full_result_knn = tree.searchkNN(target, FIND_NEARBY)
 
 print("Ranking of nearest points:")
 for i in range (len(nearest_hits)):
@@ -80,11 +88,12 @@ pos = graphviz_layout(gr, prog="twopi")
 ## adapted from
 ## https://stackoverflow.com/questions/27030473/how-to-set-colors-for-nodes-in-networkx
 colors = []
+
 for node in gr:
     if (node in nearest_hits):
-        colors.append("red")
+        colors.append(SUCCESS_COLOR)
     else:
-        colors.append("green")
+        colors.append(FAIL_COLOR) # todo: apply a color gradient for close -> far
 
 nx.draw_networkx(gr, node_color=colors, pos=pos)
 plt.show()
