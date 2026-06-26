b74cb5675acc6eae92ab56680bb437bfbf03b0b0
Tianyu Qiu <139846244+ELTQ@users.noreply.github.com>
2026-06-25 18:58:51 -0600

    Refactor mpeg7_to_sdf to use PIL and filter .gif files
    Updated image processing to use PIL for loading and resizing. Added check to process only .gif files.

--- tosdf.py
+++ tosdf.py
@@ -1,8 +1,8 @@
-import scipy
-from mpeg7 import *
+import scipy.ndimage
 import numpy as np
 import cv2
 import os
+from PIL import Image
 
 mpeg7_data = 'C:\\Users\\qiuel\\summerresearch\\part1\\mpeg7'
 
@@ -10,15 +10,18 @@ mpeg7_sdfs = "C:\\Users\\qiuel\\summerresearch\\part1\\mpeg7_sdfs"
 
 
 def mpeg7_to_sdf(image):
-
-    img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    img = Image.open(image)
+    img = img.convert('L')
+    img = img.resize((256, 256))
+    img = np.array(img)
     inside_dist = scipy.ndimage.distance_transform_edt(img)
     outside_dist = scipy.ndimage.distance_transform_edt(cv2.bitwise_not(img))
     sdf = outside_dist - inside_dist
     return sdf
 
-
-for img in os.listdir(mpeg7_data):    
-    sdfs = mpeg7_to_sdf(img)
+for img in os.listdir(mpeg7_data):
+    if not img.endswith('.gif'):
+        continue
+    sdfs = mpeg7_to_sdf(os.path.join(mpeg7_data, img))
+    print(sdfs.shape, img)
     np.save(os.path.join(mpeg7_sdfs, img.split('.')[0] + '.npy'), sdfs)
-
