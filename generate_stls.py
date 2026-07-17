015d105016453daf03e0a7339889ba2297d2444d
Tianyu Qiu <139846244+ELTQ@users.noreply.github.com>
2026-07-16 19:58:11 -0600

    Modify sphere generation to use van der Waals radii

--- generate_stls.py
+++ generate_stls.py
@@ -4,28 +4,29 @@ import os
 import trimesh
 import random
 
-# generate trimesh spheres with radius r at x, y, z position
-def generate_sphere(r, x, y, z):
-    sphere = trimesh.creation.icosphere(radius=r)
+# generate spheres with vdw radiis at x, y, z position
+def generate_sphere(x, y, z):
+    vdw_radii = {"H": 1.1, "C": 1.7, "N":1.55, "O":1.52, "S":1.8, "P": 1.8}
+    radii = random.choice(list(vdw_radii.values()))
+    sphere = trimesh.creation.icosphere(radius=radii)
     sphere.apply_translation([x,y,z])
     return sphere
 
-# generate return a list of n spheres within range 0 to 1 with radius between 0 to 1
+# generate return a list of n spheres within range 0 to 1 with random vdw radii
 def rand_sphere_union(n):
     spheres = []
     for i in range(n):
         xi = random.uniform(0,1)
         yi = random.uniform(0,1)
         zi = random.uniform(0,1)
-        ri = random.uniform(0,1)
-        spheres.append(generate_sphere(ri, xi, yi, zi))
+        spheres.append(generate_sphere(xi, yi, zi))
     return spheres
 
 
 union_dir = "C:\\Users\\qiuel\\summerresearch\\part1\\unions"
 
-# generate 10 union shape each made with 10 spheres
-for i in range(10):
+# generate 20 union shapes each made with 20 spheres
+for i in range(20):
     spheres = rand_sphere_union(10)
     un = trimesh.boolean.union(spheres)
     un.export(os.path.join(union_dir, "union" + str(i) + ".stl"))
